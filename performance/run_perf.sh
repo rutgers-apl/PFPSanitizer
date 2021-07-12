548b00dd08f4a24d3e81ccba09722ae0fb406ba5
Sangeeta <sc1696@scarletmail.rutgers.edu>
2021-07-12 10:57:08 -0400

    removed spec benchmarks

--- performance/run_perf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python run_spec.py
-python get_slowdown.py
-python get_speedup.py
--- performance/run_perf_short.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python run_spec_short.py
-python get_slowdown_short.py
-python get_speedup_short.py
